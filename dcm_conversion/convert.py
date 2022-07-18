98e597d75544f041244da9f5874371a19c94e6fb
Nathan Muncy <nathan.muncy@duke.edu>
2022-07-18 11:01:44 -0400

    added defacing function

--- dcm_conversion/convert.py
+++ dcm_conversion/convert.py
@@ -1,6 +1,13 @@
-"""Title.
+"""Convert DICOMs to NIfTI files.
 
-Desc.
+Use Chris Rorden's dcm2niix to convert DICOMs to NIfTI files. Also rename
+files and restructure directory organizaton for BIDS compliance.
+
+Notes
+-----
+Assumes flat DICOM organization.
+BIDS file naming is EmoRep specific (_switch_name).
+Assumes T1w exist for each session.
 """
 import os
 import sys
@@ -27,7 +34,7 @@ def _error_msg(msg, stdout, stderr):
     print(textwrap.dedent(error_message))
 
 
-def _switch_name(old_name, subj, sess, task="", run: str = ""):
+def _switch_name(old_name, subid, sess, task="", run: str = ""):
     """Determine EMOREP file types.
 
     Use default filename string output by dcm2niix to
@@ -38,7 +45,7 @@ def _switch_name(old_name, subj, sess, task="", run: str = ""):
     old_name : str
         Split dcm2niix name, preceding year date
         e.g. DICOM_EmoRep_anat for DICOM_EmoRep_anat_20220101_foo.nii.gz
-    subj : str
+    subid : str
         Subject identifier, BIDS label
     sess : str
         BIDS-formatted session string
@@ -53,7 +60,7 @@ def _switch_name(old_name, subj, sess, task="", run: str = ""):
         [0] = scan type (anat, func, fmap)
         [1] = BIDS-formatted file name, sans extension
     """
-    base_str = f"sub-{subj}_{sess}"
+    base_str = f"sub-{subid}_{sess}"
     name_dict = {
         "DICOM_EmoRep_anat": ("anat", f"{base_str}_T1w"),
         f"DICOM_EmoRep_run{run}": (
@@ -69,7 +76,7 @@ def _switch_name(old_name, subj, sess, task="", run: str = ""):
     return name_dict[old_name]
 
 
-def dcm2niix(subj_source, subj_raw, subj, sess, task):
+def dcm2niix(subj_source, subj_raw, subid, sess, task):
     """Conduct dcm2niix.
 
     Point dcm2niix at a DICOM directory, rename NIfTI
@@ -82,7 +89,7 @@ def dcm2niix(subj_source, subj_raw, subj, sess, task):
         Subject's DICOM directory in sourcedata
     subj_raw : Path
         Subject's rawdata directory
-    subj : str
+    subid : str
         Subject identifier
     sess : str
         BIDS-formatted session string
@@ -91,13 +98,15 @@ def dcm2niix(subj_source, subj_raw, subj, sess, task):
 
     Returns
     -------
-    None
+    t1_list : list
+        Path and name of session T1w files
 
     Raises
     ------
     FileNotFoundError
         If NIFTI files are not dected in subject rawdata.
         If the number of NIfTI and JSON are != in subject rawdata.
+        If BIDS organized T1w files are not found for the session.
     """
     # Construct and run dcm2niix cmd
     bash_cmd = f"""\
@@ -130,24 +139,29 @@ def dcm2niix(subj_source, subj_raw, subj, sess, task):
         raise FileNotFoundError("Unbalanced json and nii lists.")
 
     # Move and rename nii files
-    print(f"\t Renaming, organizing NIfTIs for sub-{subj}, {sess} ...")
+    print(f"\t Renaming, organizing NIfTIs for sub-{subid}, {sess} ...")
     for h_nii, h_json in zip(nii_list, json_list):
         old_name = os.path.basename(h_nii).split("_20")[0]
         if "run" in old_name:
             run = old_name.split("run")[1]
-            new_dir, new_name = _switch_name(old_name, subj, sess, task, run)
+            new_dir, new_name = _switch_name(old_name, subid, sess, task, run)
         else:
-            new_dir, new_name = _switch_name(old_name, subj, sess)
+            new_dir, new_name = _switch_name(old_name, subid, sess)
         new_path = os.path.join(os.path.dirname(h_nii), new_dir)
         if not os.path.exists(new_path):
             os.makedirs(new_path)
         shutil.move(h_nii, f"{new_path}/{new_name}.nii.gz")
         shutil.move(h_json, f"{new_path}/{new_name}.json")
 
+    # Make T1w list for return
+    t1_list = sorted(glob.glob(f"{subj_raw}/anat/*T1w.nii.gz"))
+    if not t1_list:
+        raise FileNotFoundError("No BIDS-organized T1w files detected.")
+
     # Update fmap json with "IntendedFor" field
-    print(f"\t Updating fmap jsons for sub-{subj}, {sess} ...")
+    print(f"\t Updating fmap jsons for sub-{subid}, {sess} ...")
     bold_list = [
-        x.split(f"sub-{subj}/")[1]
+        x.split(f"sub-{subid}/")[1]
         for x in sorted(glob.glob(f"{subj_raw}/func/*bold.nii.gz"))
     ]
     fmap_json = glob.glob(f"{subj_raw}/fmap/*json")[0]
@@ -156,3 +170,5 @@ def dcm2niix(subj_source, subj_raw, subj, sess, task):
     fmap_dict["IntendedFor"] = bold_list
     with open(fmap_json, "w") as jf:
         json.dump(fmap_dict, jf)
+
+    return t1_list
