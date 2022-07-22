ab2b805a3953edcd137b289face0a1707e1c5cf6
Nathan Muncy <nathan.muncy@duke.edu>
2022-07-22 16:54:35 -0400

    updated install and settings files

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# develop: python setup.py develop
-# install: python setup.py install
 from setuptools import setup, find_packages
 
 setup(
@@ -11,4 +9,12 @@ setup(
             "dcm_conversion=dcm_conversion.cli:main",
         ]
     },
+    install_requires=[
+        "nibabel==4.0.1",
+        "numpy==1.23.1",
+        "pandas==1.4.3",
+        "pydeface==2.0.2",
+        "pytest==7.1.2",
+        "setuptools==45.2.0",
+    ],
 )
